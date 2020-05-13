b89f59347a2dc740b91e3bdf018650220a6c54ae
ethanmjansen <ethanmjansen@users.noreply.github.com>
2020-05-12 22:15:38 -0600

    Took out made if name equals main function a pass

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -80,7 +80,7 @@ class DoublyLinkedList:
         else:
             new_node.prev = self.tail
             self.tail.next = new_node
-            self.tail = new_node     
+            self.tail = new_node
 
     """Removes the List's current tail node, making the 
     current tail's previous node the new tail of the List.
@@ -94,10 +94,11 @@ class DoublyLinkedList:
     List and inserts it as the new head node of the List."""
     def move_to_front(self, node):
         if node is self.head:
-            return None
-        value = node.value
-        self.delete(node)
-        self.add_to_head(value)
+            return 
+        else:
+            value = node.value
+            self.delete(node)
+            self.add_to_head(value)
         
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
@@ -150,8 +151,7 @@ class DoublyLinkedList:
         print(self.temp) 
 
 if __name__ == '__main__':
-    my_list = DoublyLinkedList()
-    my_list.remove_from_tail()
+    pass
 
 
     
